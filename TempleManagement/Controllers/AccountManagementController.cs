840d3f31e11b88698312935cfa377bd8181ac525
Jerry Lin <jerry8g@gmail.com>
2026-06-22 11:04:15 +0800

    下次followerlist調整details

--- TempleManagement/Controllers/AccountManagementController.cs
+++ TempleManagement/Controllers/AccountManagementController.cs
@@ -6,7 +6,7 @@ using TempleManagement.Models.DBManager;
 
 namespace TempleManagement.Controllers
 {
-    public class AcidManagementController : Controller
+    public class AccountManagementController : Controller
     {
         public IActionResult Index()
         {
@@ -33,13 +33,13 @@ namespace TempleManagement.Controllers
                 return Json(new { success = false, message = "資料驗證失敗" });
             }
 
-            Debug.WriteLine($"NewAcid_info_check:{JsonSerializer.Serialize(info)}");
+            Debug.WriteLine($"NewAccount_info_check:{JsonSerializer.Serialize(info)}");
 
             try
             {
-                AcidManagement_DBManager dbManager = new AcidManagement_DBManager();
-                Debug.WriteLine("AcidManagement_DBManager");
-                await dbManager.newAcid(info);
+                AccountManagement_DBManager dbManager = new AccountManagement_DBManager();
+                Debug.WriteLine("AccountManagement_DBManager");
+                await dbManager.newAccount(info);
 
 
                 return Json(new { success = true, name = info.Name });
@@ -75,9 +75,9 @@ namespace TempleManagement.Controllers
 
             try
             {
-                AcidManagement_DBManager dbManager = new AcidManagement_DBManager();
-                Debug.WriteLine("AcidManagement_DBManager start");
-                List<Admin> infos = await dbManager.getAcid(info);
+                AccountManagement_DBManager dbManager = new AccountManagement_DBManager();
+                Debug.WriteLine("AccountManagement_DBManager start");
+                List<Admin> infos = await dbManager.getAccount(info);
 
                 if (infos.Count == 1)
                 {
