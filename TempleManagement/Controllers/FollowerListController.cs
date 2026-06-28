6668941d390319fb0b2a08c2f917be02fe6f4aa2
Jerry Lin <jerry8g@gmail.com>
2026-06-28 17:27:38 +0800

    下次followerList完善

--- TempleManagement/Controllers/FollowerListController.cs
+++ TempleManagement/Controllers/FollowerListController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
 using TempleManagement.Models;
 using TempleManagement.Models.DBManager;
 
@@ -12,6 +14,29 @@ namespace TempleManagement.Controllers
             List<BasicInfo> Info = await db.getBasicInfo(); //已改成 postgresql
             return View(Info);
 
+        }
+
+        // 按下新增捐獻 顯示列表 (為初始導入)
+        // 後，把篩選的option加入此函式，擴建多樣性
+        public async Task<IActionResult> ShowOption()
+        {
+            BasicInfo_DBManager db = new BasicInfo_DBManager();
+            List<BasicInfo> Info = await db.getBasicInfo(); //已改成 postgresql
+
+            Debug.WriteLine("showoption()");
+            Debug.WriteLine($"{JsonSerializer.Serialize(Info)}");
+            return PartialView("_Show_details", Info);
+        }
+
+        public async Task<IActionResult> Detail(int mid)
+        {
+            BasicInfo_DBManager db = new BasicInfo_DBManager();
+            List<BasicInfo> Info = await db.getBasicInfo(member_id:mid); //抓單筆
+
+            Debug.WriteLine("show details");
+            Debug.WriteLine($"{JsonSerializer.Serialize(Info)}");
+
+            return PartialView("_Show_details", Info[0]);
         }
     }
 }
